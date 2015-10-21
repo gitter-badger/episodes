524a085615d67f04307ea0b6f2b78b716152e82a
mumbleanswer <ma.lepoleswebforce3@gmail.com>
2015-10-21 16:23:35 +0200

    Ajout page légal

---
+++ legal.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+      <meta charset="UTF-8">
+      <title>Mentions legales - Episodes</title>
+      <!-- Slider header -->
+      <link rel="stylesheet" href="css/cssslider_files/csss_engine1/style.css">
+      <!-- Favicon -->
+      <link rel="icon" href="images/logo_icon.png" type="image/png">
+      <!-- Hover.css -->
+      <link rel="stylesheet" href="Hover-master/css/hover.css">
+      <!--  CSS -->
+      <link rel="stylesheet" type="text/css" href="css/normalize.css">
+      <link rel="stylesheet" type="text/css" href="css/style.css">
+      <!-- Font -->
+       <link href='https://fonts.googleapis.com/css?family=Pacifico' rel='stylesheet' type='text/css'>
+      <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,600,700' rel='stylesheet' type='text/css'>
+      <link rel="stylesheet" type="text/css" href="css/font-awesome-4.3.0/css/font-awesome.css">
+      <!-- Jquery -->
+      <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+      <!--[if IE]>
+      <script type="text/javascript" src="js/modernizr.custom.78869.js"></script>
+      <![endif]-->
+</head>
+<body>
+	      <?php 
+         include('includes/db.php');
+         include("header.php"); ?>
+
+
+<h4>Identification Editeur : </h4>
+<p>Mathilde, Présidente</p>
+
+<p>PoleS - WebForce 3</p>
+<p>11, Allée saint exupery</p>
+<p>92390 Villeuneuve-La-Garenne</p>
+
+<h4>Contacts :</h4>
+<p>Tel: +33 1 47.92.75.25</p>
+<p>Fax: +33 1 47.92.88.67</p>
+
+<h4>SIREN :</h4> <p>411 659 675</p>
+
+<h4>SIRET :</h4> <p>411 659 675 00028</p>
+
+<h4>CAPITAL :</h4> <p>160 000 €</p>
+
+<h4>Directeur de la publication :</h4>
+<p>Thomas</p>
+
+<h4>Identification Hébergeur :</h4>
+
+<p> PoleS - WebForce 3</p>
+<p>11, Allée saint exupery</p>
+<p>92390 Villeuneuve-La-Garenne</p>
+
+
+Cédric SIRE, Président
+
+<h4>Vie privée et cookies</h4>
+
+<h4>Propriété Intellectuelle</h4>
+
+<h4>Conditions Générales</h4>
+
+
+<?php include("footer.php"); ?>
+</body>
+
+</html>

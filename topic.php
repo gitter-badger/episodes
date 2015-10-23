12eefc4eb330cfc2d3759d5f0b94783dc3372c23
tomtomgo92 <tm.lepoleswebforce3@gmail.com>
2015-10-23 17:01:57 +0200

    topic (page topic) .php

---
+++ topic.php
@@ -0,0 +1,23 @@
+<?php 
+
+ include('includes/db.php');
+
+	$template	= $twig -> loadTemplate('topic.html');
+
+	$profil		= $forum -> selectUser($_GET['id']);
+	$topicsUser	= $forum -> afficherTopicUser($_GET['id']);
+	$topic		= afficherTopic();
+	$messages	= selectMessages($topics[0]['id']);
+
+	echo $template->render([
+		'userSession'	=> $forum -> selectUser($_SESSION['users']['id']),
+		'profil'		=> $profil, 
+		'topics'		=> $topics, 
+		'topicsUser'	=> $topicsUser, 
+		'categorie'		=> $categorie, 
+		'auteurTopic'	=> $auteurTopic, 
+		'profil'		=> $profil,
+		'messages'		=> $messages,
+		'forum'			=> $forum
+		]);
+

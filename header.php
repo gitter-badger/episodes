8d06da0065f8f908473e45f4b7a451c48ddcc564
tomtomgo92 <tm.lepoleswebforce3@gmail.com>
2015-10-23 11:13:41 +0200

    header encore...

--- header.php
+++ header.php
@@ -1,8 +1,8 @@
 <?php 
 
-    include('includes/bd.php');
+    include('includes/db.php');
 
-$template = $twig->loadTemplate('header_liste.html');
+$template = $twig->loadTemplate('header.html');
          $users  = $forum->selectUsers();
          
         echo $template->render(['users' => $users[0]['pseudo'], 'userId'=>$users[0]['id'], 'users' => $users[0]['avatar']] );

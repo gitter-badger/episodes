4d9e8679548ee0a2d69486b3bb666be9306c75cb
mumbleanswer <ma.lepoleswebforce3@gmail.com>
2015-11-27 14:33:18 +0100

    s

--- categories.php
+++ categories.php
@@ -2,7 +2,7 @@
 
  include('includes/db.php');
 
-	$template 	= $twig->loadTemplate('categories.html');
+	$template 	= $twig->loadTemplate('categorie.html');
     $listeTopicsCategorie=$forum->listeTopicsCategorie($_GET['id']);
     $titreCategorie=$forum->afficherCategorie($_GET['id']);  
       
